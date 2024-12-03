4fdf868fe6b2de55df9c865ced0c425a3654b64c
musab03 <musab.khurram@gmail.com>
2024-12-04 01:28:02 +0500

    add: default button and prevent scroll

---
+++ app/types/telegram-webapp.d.ts
@@ -0,0 +1,21 @@
+interface TelegramWebApp {
+    ready: () => void;
+    expand?: () => void; // Optional expand method
+    close: () => void;
+    BackButton: {
+      show: () => void;
+      hide: () => void;
+      onClick: (callback: () => void) => void;
+    };
+  }
+  
+  interface Telegram {
+    WebApp: TelegramWebApp;
+  }
+  
+  declare global {
+    interface Window {
+      Telegram: Telegram;
+    }
+  }
+  
