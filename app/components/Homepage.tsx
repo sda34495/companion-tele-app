16c97a9d27840f43048ac1141ca562c35f67571a
musab03 <musab.khurram@gmail.com>
2024-12-03 22:14:53 +0500

    add: setting page

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,47 +1,27 @@
-'use client'
+"use client";
 import Link from "next/link";
 import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-
-      <div className="bg-[#efeff3] relative  w-full mx-auto rounded-t-2xl sm:max-w-xs  flex-grow">
-        
-        <div className="bg-[#efeff3] w-full  p-6 ">
-          <div className="flex absolute top-36 right-0 left-0 flex-col px-2 items-center">
-            {/* Icon Placeholder */}
-            <div className="w-14 h-14 bg-[#00b0ff] rounded-2xl mb-4 mt-10"></div>
-            {/* Title */}
-            <h2 className="text-xl font-semibold text-gray-800 mb-1 tracking-tight">
-              Cloud Companions
-            </h2>
-            {/* Subtitle */}
-            <p className="text-sm  text-[#787878] px-4 text-center mb-6">
-              With Cloud Companions you can explore and chat with AI Characters
-            </p>
-            {/* Buttons */}
-            <div className="divide-y divide-gray-200 border border-gray-200 rounded-lg w-full bg-white">
-              <Link href='./setting'>
-              <button className="flex items-center justify-between w-full px-4 py-1.5 text-left text-black text-sm font-medium">
+    <div className="bg-[#efeff3] relative  w-full mx-auto rounded-t-2xl sm:max-w-xs  flex-grow">
+      <div className="bg-[#efeff3] w-full  p-6 ">
+        <div className="flex absolute top-36 right-0 left-0 flex-col px-2 items-center">
+          {/* Icon Placeholder */}
+          <div className="w-14 h-14 bg-[#00b0ff] rounded-2xl mb-4 mt-10"></div>
+          {/* Title */}
+          <h2 className="text-xl font-semibold text-gray-800 mb-1 tracking-tight">
+            Cloud Companions
+          </h2>
+          {/* Subtitle */}
+          <p className="text-sm  text-[#787878] px-4 text-center mb-6">
+            With Cloud Companions you can explore and chat with AI Characters
+          </p>
+          {/* Buttons */}
+          <div className="divide-y divide-gray-200 border border-gray-200 rounded-lg w-full bg-white">
+            <Link href="./setting">
+              <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
                 Personal Settings
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-[#787878]"
-                  >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                    />
-                </svg>
-              </button>
-                    </Link>
-              <button className="flex items-center justify-between w-full px-4 py-1.5 text-left text-black text-sm font-medium">
-                Purchase Hearts
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -57,8 +37,28 @@ const HomePage: React.FC = () => {
                   />
                 </svg>
               </button>
-              <Link href='./reset'>
-              <button className="flex items-center justify-between w-full px-4 py-1.5 text-left text-black text-sm font-medium">
+            </Link>
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Purchase Hearts
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+              
+            </button>
+            <hr />
+            <Link href="./reset">
+              <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
                 Reset Chat
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -67,32 +67,28 @@ const HomePage: React.FC = () => {
                   strokeWidth="1.5"
                   stroke="currentColor"
                   className="size-5 text-[#787878]"
-                  >
+                >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                    />
+                  />
                 </svg>
               </button>
-                    </Link>
-            </div>
-            <hr className=" border-gray-300 w-full mt-4 " />
-
-            {/* Bottom Button */}
-            
+            </Link>
           </div>
-         
+          <hr className=" border-gray-300 w-full mt-4 " />
 
-              <Link href="./explore">
-            <button   className="w-full  bg-[#007cff] absolute bottom-0 right-0 left-0 text-white py-3  rounded-lg text-sm font-semibold ">
-              Explore AI Characters
-            </button>
-              </Link>
-      
+          {/* Bottom Button */}
         </div>
-      </div>
 
+        <Link href="./explore">
+          <button className="w-full  bg-[#007cff] absolute bottom-0 right-0 left-0 text-white py-3  rounded-lg text-sm font-semibold ">
+            Explore AI Characters
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- app/components/Setting.tsx
+++ app/components/Setting.tsx
@@ -1,90 +1,200 @@
-'use client'
-import Image from "next/image";
-import React from "react";
-import bgImage from "@/public/Albert_Einstein.jpg";
+'use client';
+import Link from 'next/link';
+import React from 'react';
 
-const Setting: React.FC = () => {
-  return (
-      <div className="bg-[#efeff3] w-full sm:max-w-xs mx-auto flex-grow rounded-t-2xl">
-        
-        
+const Settings: React.FC = () => {
 
   
-        <div className="bg-[#efeff3] w-full  p-6 ">
-          <div className="flex flex-col items-center">
-            {/* Icon Placeholder */}
-            <div className="text-center mb-10">
-              <div className="w-16 h-16 overflow-hidden rounded-2xl mx-auto ">
-              <Image src={bgImage} alt="Albert Einstein" className="w-full" />
+  return (
+    <div className="bg-[#efeff3] relative w-full sm:max-w-xs mx-auto flex-grow">
+      <div className="py-6 px-4">
 
-              </div>
-              <h2 className="text-lg font-bold text-black">Albert</h2>
-              <p className="text-gray-500 tracking-tight px-6 text-xs">
-                This is a short description for the AI character
-              </p>
-            </div>
-
-            <div className="w-full max-w-sm space-y-6">
-        {/* Personality Section */}
+        {/* Profile Details */}
         <div>
-          <h3 className="text-xs ml-1 text-gray-500 font-medium mb-1">
-            PERSONALITY
+          <h3 className="text-xs text-gray-500 font-medium mb-2 ">
+            PROFILE DETAILS
           </h3>
-          <p className="text-sm bg-white rounded-lg px-1 py-2 text-gray-800">
-            Lorem ipsum dolor sit amet, consectetur
-          </p>
-        </div>
-
-        {/* Occupation Section */}
-        <div>
-          <h3 className="text-xs ml-1 text-gray-500 font-medium mb-1">OCCUPATION</h3>
-          <p className="text-sm bg-white rounded-lg px-1 py-2 text-gray-800">
-            Lorem ipsum dolor sit amet, consectetur
-          </p>
-        </div>
-
-        {/* About Section */}
-        <div>
-          <h3 className="text-xs ml-1 text-gray-500 font-medium mb-1">ABOUT</h3>
-          <p className="text-sm bg-white rounded-lg px-1 py-2 text-gray-800">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <p className="text-sm bg-white rounded-lg px-1 py-2 text-gray-800">
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p className="text-sm bg-white rounded-lg px-1 py-2 text-gray-800">
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
-             </div>
-          
-
-
-            <hr className=" border-gray-300 w-full mt-4" />
-
-            {/* Bottom Button */}
-
-            <button className="w-full bg-[#007cff] text-white py-2 rounded-lg text-sm font-semibold mt-6">
-            Chat now
+          <div className="divide-y divide-gray-200 border border-gray-200 rounded-lg bg-white">
+            <button className="flex space-x-0 items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Name
+              <span className="text-gray-500 ">Lennard</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </button>
-
-            <button className="w-full bg-white text-black py-2 rounded-lg text-sm font-semibold mt-2">
-              <a href="./explore">Back to all Characters</a>
-            
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Gender
+              <span className="text-gray-500">Male</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </button>
-
-
-
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Email
+              <span className="text-gray-500">mail@lennard-klein.com</span>
+               <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Language
+              <span className="text-gray-500">English</span>
+               <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
           </div>
         </div>
+
+        {/* Chat Settings */}
+        <div className="mt-6">
+          <h3 className="text-xs text-gray-500 font-medium mb-2">
+            CHAT SETTINGS
+          </h3>
+          <div className="flex items-center justify-between px-4 py-3 border border-gray-200 rounded-lg bg-white">
+            <span className="text-sm text-gray-900">Notifications</span>
+            <label className="relative inline-flex items-center cursor-pointer">
+<input type="checkbox" value="" className="sr-only peer"/>
+<div className="w-9 h-5 bg-gray-200 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-green-500 hover:peer-checked:bg-green-500"></div>
+</label>
+          </div>
+          <p className="text-xs text-gray-500 mt-2">
+            Your AI Companion may proactively send messages to spark
+            conversations and keep you connected.
+          </p>
+        </div>
+
+        {/* Help & Legal */}
+        <div className="mt-6">
+          
+          <h3 className="text-xs text-gray-500 font-medium mb-2">
+            HELP & LEGAL
+          </h3>
+          <div className="divide-y divide-gray-200 border border-gray-200 rounded-lg bg-white">
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Imprint
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-[#787878]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+            </button>
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Privacy Policy
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-[#787878]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+            </button>
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Terms of Use
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-[#787878]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+            </button>
+            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              Contact Support
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-[#787878]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+            </button>
+          </div>
+        </div>
+
+        {/* Affiliate Button */}
+        <div className="mt-6 ">
+          <button className="w-full bg-[#007cff] absolute bottom-0 left-0 right-0 text-white py-3 rounded-lg text-sm font-medium">
+            Become an Affiliate
+          </button>
+        </div>
       </div>
+    </div>
   );
 };
 
-export default Setting;
+export default Settings;
