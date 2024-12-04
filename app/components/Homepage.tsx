44cbf1c54892c0be685adf19de5567148ce6f9c7
musab03 <musab.khurram@gmail.com>
2024-12-04 14:23:40 +0500

    fix: homepage

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -5,8 +5,8 @@ import React from "react";
 const HomePage: React.FC = () => {
   return (
     <div className="bg-[#efeff3] relative  w-full mx-auto sm:max-w-xs  flex-grow">
-      <div className="bg-[#efeff3] w-full  p-6 ">
-        <div className="flex absolute top-36 right-0 left-0 flex-col px-3 items-center">
+      <div className="bg-[#efeff3] w-full  ">
+        <div className="flex absolute top-36 right-0 left-0 flex-col  items-center">
           {/* Icon Placeholder */}
           <div className="w-14 h-14 bg-[#00b0ff] rounded-2xl mb-4 mt-10"></div>
           {/* Title */}
@@ -18,9 +18,11 @@ const HomePage: React.FC = () => {
             With Cloud Companions you can explore and chat with AI Characters
           </p>
           {/* Buttons */}
-          <div className="divide-y divide-gray-200 border border-gray-200 rounded-lg w-full bg-white">
+          <div className="w-full px-3">
+
+          <div className="divide-y px-3 divide-gray-200 border border-gray-200 rounded-lg w-full bg-white">
             <Link href="./setting">
-              <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              <button className="flex items-center justify-between w-full  py-3 text-sm font-medium text-left text-gray-900">
                 Personal Settings
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -34,14 +36,14 @@ const HomePage: React.FC = () => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                  />
+                    />
                 </svg>
               </button>
               <hr />
             </Link>
 
             <Link href='./payment'>
-            <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+            <button className="flex items-center justify-between w-full  py-3 text-sm font-medium text-left text-gray-900">
               Purchase Hearts
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -62,7 +64,7 @@ const HomePage: React.FC = () => {
                   </Link>
             <hr />
             <Link href="./reset">
-              <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
+              <button className="flex items-center justify-between w-full  py-3 text-sm font-medium text-left text-gray-900">
                 Reset Chat
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -71,15 +73,16 @@ const HomePage: React.FC = () => {
                   strokeWidth="1.5"
                   stroke="currentColor"
                   className="size-5 text-[#787878]"
-                >
+                  >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                  />
+                    />
                 </svg>
               </button>
             </Link>
+                    </div>
           </div>
           <hr className=" border-gray-300 w-full mt-4 " />
 
