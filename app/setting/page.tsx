c95c73acdf0d44f86a7b30b6dfad1a93da151bd7
musab03 <musab.khurram@gmail.com>
2024-12-04 15:26:37 +0500

    add: lazy loading on setting

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import React from 'react'
 import Setting from '../components/Setting'
+import dynamic from 'next/dynamic';
 
 function page() {
+  const LazySection = dynamic(() => import('../../app/components/Setting'), { ssr: false });
+
   return (
-    <Setting/>
+    <LazySection/>
   )
 }
 
