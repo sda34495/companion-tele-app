e479903107e1eba0d1241d973c7de0014110376f
musab03 <musab.khurram@gmail.com>
2024-12-05 23:12:11 +0500

    add link

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -88,9 +88,11 @@ const HomePage: React.FC = () => {
       {/* Button at the bottom */}
       <div className="mt-auto px-3">
         <div className="divide-y divide-gray-200 border border-gray-200 rounded-lg bg-white">
+          <Link href='/explore'>
           <button className="flex bg-[#007cff] justify-center w-full px-4 py-3 mb-3 text-sm font-semibold text-center rounded-lg text-white">
             Explore AI Characters
           </button>
+          </Link>
         </div>
       </div>
     </div>
