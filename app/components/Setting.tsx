95b0bffacd8af1d3d8c0f4be8a314fefb0e8ab7e
musab03 <musab.khurram@gmail.com>
2024-12-05 22:10:30 +0500

    add: bg to check button

--- app/components/Setting.tsx
+++ app/components/Setting.tsx
@@ -1,16 +1,13 @@
-'use client';
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React from "react";
 
 const Settings: React.FC = () => {
-
-  
   return (
     <div className="bg-[#efeff3] w-full sm:max-w-xs mx-auto flex-grow">
-      <div className=" py-6 ">
-
+      <div className=" py-6 flex-col w-full sm:max-w-xs mx-auto   ">
         {/* Profile Details */}
-        <div className='px-3'>
+        <div className="px-3">
           <h3 className="text-xs ml-2 text-gray-500 font-medium mb-2 ">
             PROFILE DETAILS
           </h3>
@@ -54,7 +51,7 @@ const Settings: React.FC = () => {
             <button className="flex items-center justify-between w-full px-2 py-3 text-sm font-medium text-left text-gray-900">
               Email
               <span className="text-gray-500">mail@lennard-klein.com</span>
-               <svg
+              <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -72,7 +69,7 @@ const Settings: React.FC = () => {
             <button className="flex items-center justify-between w-full px-2 py-3 text-sm font-medium text-left text-gray-900">
               Language
               <span className="text-gray-500">English</span>
-               <svg
+              <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -90,7 +87,7 @@ const Settings: React.FC = () => {
           </div>
         </div>
 
-        <hr className='mt-5 border-gray-300' />
+        <hr className="mt-5 border-gray-300" />
 
         {/* Chat Settings */}
         <div className="mt-5 px-3">
@@ -100,9 +97,9 @@ const Settings: React.FC = () => {
           <div className="flex items-center justify-between px-4 py-3 border border-gray-200 rounded-lg bg-white">
             <span className="text-sm text-gray-900">Notifications</span>
             <label className="relative inline-flex items-center cursor-pointer">
-<input type="checkbox" value="" className="sr-only peer"/>
-<div className="w-9 h-5 bg-gray-200 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-green-500 hover:peer-checked:bg-green-500"></div>
-</label>
+              <input type="checkbox" value="" className="sr-only peer" />
+              <div className="w-9 h-5 bg-gray-200 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-green-500 hover:peer-checked:bg-green-500"></div>
+            </label>
           </div>
           <p className="text-xs mx-3 text-gray-500 mt-2">
             Your AI Companion may proactively send messages to spark
@@ -110,12 +107,10 @@ const Settings: React.FC = () => {
           </p>
         </div>
 
-        <hr className='mt-5 border-gray-300' />
-
+        <hr className="mt-5 border-gray-300" />
 
         {/* Help & Legal */}
         <div className="mt-6 px-3">
-          
           <h3 className="text-xs ml-2 text-gray-500 font-medium mb-2">
             HELP & LEGAL
           </h3>
@@ -123,82 +118,81 @@ const Settings: React.FC = () => {
             <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
               Imprint
               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-[#787878]"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                  />
-                </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </button>
             <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
               Privacy Policy
               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-[#787878]"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                  />
-                </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </button>
             <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
               Terms of Use
               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-[#787878]"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                  />
-                </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </button>
             <button className="flex items-center justify-between w-full px-4 py-3 text-sm font-medium text-left text-gray-900">
               Contact Support
               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-[#787878]"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                  />
-                </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-5 text-[#787878]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
             </button>
           </div>
-        </div>
 
-        <hr className='mt-5 border-gray-300' />
-
-        {/* Affiliate Button */}
-        <div className="mt-5 px-3">
-          <button className="w-[335px] mx-auto bg-[#007cff] fixed  bottom-0 inset-x-0 left-0 right-0 text-white py-3 rounded-lg text-sm font-semibold sm:max-w-xs">
-            Become an Affiliate
-          </button>
         </div>
+          <hr className="mt-5 border-gray-300" />
+
+          <div className="mt-5 bg-red-500 ">
+            <button className="w-[335px] flex max-w-sm mx-auto bg-black items-end justify-center py-3  text-white  rounded-lg text-sm font-semibold ">
+              Become an Affiliate
+            </button>
+          </div>
       </div>
     </div>
   );
