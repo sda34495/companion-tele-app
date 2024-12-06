1ae3be99bd1426e8714bf9388feb1ebc9fa9ba5e
musab03 <musab.khurram@gmail.com>
2024-12-06 19:22:14 +0500

    fix:home page

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -86,7 +86,7 @@ const HomePage: React.FC = () => {
       
           <hr className="border-gray-300 w-full mt-4 mb-5" />
       {/* Button at the bottom */}
-      <div className="sticky bottom-0 bg-[#efeff3] w-full px-3">
+      <div className=" bg-[#efeff3] w-full px-3">
         <Link href="/explore">
           <button className="flex bg-[#007cff] justify-center w-full px-4 py-3 mb-5 font-semibold text-center rounded-lg text-white">
             Explore AI Characters
