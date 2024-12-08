5ea567f4bbce32afb9e26610ef4f90de5f83b773
musab03 <musab.khurram@gmail.com>
2024-12-08 20:41:29 +0500

    take it back to where it was

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,41 +1,8 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 const HomePage: React.FC = () => {
-
-  const buttonRef1 = useRef<HTMLButtonElement>(null);
-  const buttonRef2 = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    const addHapticFeedback = (buttonRef: React.RefObject<HTMLButtonElement>) => {
-      if (window.Telegram?.WebApp.HapticFeedback && buttonRef.current) {
-        buttonRef.current.addEventListener("click", () => {
-          window.Telegram?.WebApp.HapticFeedback.impactOccurred("light");
-        });
-      }
-    };
-
-    // Attach haptic feedback to buttons
-    addHapticFeedback(buttonRef1);
-    addHapticFeedback(buttonRef2);
-
-    // Cleanup event listeners on unmount
-    return () => {
-      if (buttonRef1.current) {
-        buttonRef1.current.removeEventListener("click", () => {
-          window.Telegram?.WebApp.HapticFeedback.impactOccurred("light");
-        });
-      }
-      if (buttonRef2.current) {
-        buttonRef2.current.removeEventListener("click", () => {
-          window.Telegram?.WebApp.HapticFeedback.impactOccurred("light");
-        });
-      }
-    };
-  }, []);
-
-
   return (
     <div className="bg-[#efeff3] relative w-full mx-auto sm:max-w-xs flex flex-col h-screen">
       <div className=" pb-20 flex-grow">
@@ -130,9 +97,7 @@ const HomePage: React.FC = () => {
       <div className="fixed bottom-0 left-0 right-0 bg-[#f2f2f2] border-t border-gray-200 py-2 px-3 w-full mx-auto pb-[env(safe-area-inset-bottom)] ">
         <div className="pb-5">
           <Link href="/explore">
-            <button className="flex mx-auto bg-[#007cff] items-center justify-center px-4 py-3 w-full text-white rounded-xl text-lg font-semibold"
-            ref={buttonRef1}
-            >
+            <button className="flex mx-auto bg-[#007cff] items-center justify-center px-4 py-3 w-full text-white rounded-xl text-lg font-semibold">
               Explore AI Characters
             </button>
           </Link>
