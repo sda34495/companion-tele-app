470870f8b244d5ec64fa1ac8179b36faa16b8018
musab03 <musab.khurram@gmail.com>
2024-12-08 15:15:11 +0500

    add border

--- app/components/Setting.tsx
+++ app/components/Setting.tsx
@@ -309,7 +309,7 @@ const Settings: React.FC = () => {
         {/* <hr className="mt-5 border-gray-300" /> */}
 
       </div>
-        <div className="fixed bottom-0 left-0 right-0 bg-[#f2f2f2] p-5 w-full max-w-sm mx-auto">
+        <div className="fixed bottom-0 left-0 right-0 bg-[#f2f2f2] border-t border-gray-200 p-5 w-full max-w-sm mx-auto">
         <button className="flex mx-auto bg-[#007cff] items-center mb-2 justify-center py-3  w-full text-white rounded-lg text-lg font-semibold">
           Become an Affiliate
         </button>
