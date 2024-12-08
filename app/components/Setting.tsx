c68194f413f6568dd87a94ed521825136175d6a2
musab03 <musab.khurram@gmail.com>
2024-12-08 15:16:38 +0500

    remove mb

--- app/components/Setting.tsx
+++ app/components/Setting.tsx
@@ -310,7 +310,7 @@ const Settings: React.FC = () => {
 
       </div>
         <div className="fixed bottom-0 left-0 right-0 bg-[#f2f2f2] border-t border-gray-200 p-5 w-full max-w-sm mx-auto">
-        <button className="flex mx-auto bg-[#007cff] items-center mb-2 justify-center py-3  w-full text-white rounded-lg text-lg font-semibold">
+        <button className="flex mx-auto bg-[#007cff] items-center justify-center py-3  w-full text-white rounded-lg text-lg font-semibold">
           Become an Affiliate
         </button>
       </div>
