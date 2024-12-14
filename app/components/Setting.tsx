34b4a24f92e3d86eac09d4fd042c8dfd8629bfc7
musab03 <musab.khurram@gmail.com>
2024-12-14 14:38:03 +0500

    fix: setting page

--- app/components/Setting.tsx
+++ app/components/Setting.tsx
@@ -131,24 +131,30 @@ const Settings: React.FC = () => {
             HELP & LEGAL
           </h3>
           <div className=" overflow-hidden rounded-lg bg-white ">
-            <button className="flex hover:bg-gray-300 items-center justify-between w-full px-4 py-3  font-medium text-left text-gray-900">
-              Imprint
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5 text-[#787878]"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                />
-              </svg>
-            </button>
+            <Link href="https://www.cloud-companions.com/legal/imprint">
+              <button className="flex hover:bg-gray-300 items-center justify-between w-full px-4 py-3  font-medium text-left text-gray-900">
+                Imprint
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5 text-[#787878]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                  />
+                </svg>
+              </button>
+            </Link>
+
+
             <hr className="border- border-gray-200 mx-4" />
+
+            <Link href='https://www.cloud-companions.com/legal/privacy-policy'>
             <button className="flex items-center hover:bg-gray-300 justify-between w-full px-4 py-3  font-medium text-left text-gray-900">
               Privacy Policy
               <svg
@@ -162,11 +168,17 @@ const Settings: React.FC = () => {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
                 />
               </svg>
             </button>
+            </Link>
+
+
             <hr className="border- border-gray-200 mx-4" />
+
+
+            <Link href='https://www.cloud-companions.com/legal/terms-of-use'>
             <button className="flex items-center hover:bg-gray-300 justify-between w-full px-4 py-3  font-medium text-left text-gray-900">
               Terms of Use
               <svg
@@ -180,11 +192,16 @@ const Settings: React.FC = () => {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                />
+                  d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                  />
               </svg>
             </button>
+                  </Link>
+
+
             <hr className="border- border-gray-200 mx-4" />
+
+            <Link href='https://t.me/lennardklein'>
             <button className="flex items-center hover:bg-gray-300 justify-between w-full px-4 py-3  font-medium text-left text-gray-900">
               Contact Support
               <svg
@@ -194,17 +211,18 @@ const Settings: React.FC = () => {
                 strokeWidth="1.5"
                 stroke="currentColor"
                 className="size-5 text-[#787878]"
-              >
+                >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                />
+                  d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                  />
               </svg>
             </button>
+                  </Link>
           </div>
         </div>
-      
+
         {/* <hr className="mt-5 border-gray-300" /> */}
       </div>
       {/* <div className="fixed bottom-0 left-0 right-0 bg-[#f2f2f2] border-t border-gray-200 py-2 px-3 w-full mx-auto pb-[env(safe-area-inset-bottom)] ">
@@ -215,7 +233,7 @@ const Settings: React.FC = () => {
         </div>
       </div> */}
 
-      <Button name='Become an Affiliate' href='' />
+      <Button name="Become an Affiliate" href="" />
     </div>
   );
 };
